9e8f7db155cd33b30e0d680c366faa2a00949cae
lpicci96 <lpicci96@gmail.com>
2022-06-17 15:32:33 -0600

    add gain scatter

--- scripts/charts.py
+++ scripts/charts.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import country_converter as coco
 from scripts import utils, config
-from scripts.download_data import get_emdat
+from scripts.download_data import get_emdat, get_ndgain_data
 
 def drought_flood_map() -> None:
     """ """
@@ -13,4 +13,19 @@ def drought_flood_map() -> None:
     (utils.add_flourish_geometries(df)
           .pipe(utils.filter_countries, by='continent')
           .to_csv(f'{config.paths.output}/drought_flood_map.csv', index=False))
+
+
+def vuln_readi_scatter():
+    """ """
+
+    df = get_ndgain_data()
+    df = (df
+          .pipe(utils.add_income_levels)
+          .pipe(utils.add_debt_distress)
+          .dropna(subset = ['gain', 'vulnerability', 'readiness'])
+          .assign(country = lambda d: coco.convert(d.iso_code, to = 'name_short'))
+          )
+
+    return df
+
 
